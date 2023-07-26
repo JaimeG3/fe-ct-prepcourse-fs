f55d31651774343e4a852cc79238811cc4f13727
jaimitoonb <jaimegallegocaro@gmail.com>
2023-07-26 15:32:39 -0500

    homework

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,16 +6,19 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return num ** 2
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return math.pow(num, 3)
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   return Math.pow(num,exponent)
 }
 
 function redondearNumero(num) {
